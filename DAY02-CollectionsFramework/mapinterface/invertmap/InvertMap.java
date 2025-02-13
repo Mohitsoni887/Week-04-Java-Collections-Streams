535991e88fbef854844cbc1ecca4cdc923545f31
Mohit Soni <113204246+Mohitsoni887@users.noreply.github.com>
2025-02-13 13:12:54 +0530

    [Mohit Soni] Add. Invert Map Using Map Interface

--- DAY02-CollectionsFramework/mapinterface/invertmap/InvertMap.java
+++ DAY02-CollectionsFramework/mapinterface/invertmap/InvertMap.java
@@ -3,7 +3,7 @@ Invert a Map
 Invert a Map<K, V> to produce a Map<V, K>. Handle duplicate values by storing them in a list.
 Example:
 Input: {A=1, B=2, C=1} → Output: {1=[A, C], 2=[B]}.
-*/
+*/ 
 
 package com.mapinterface.invertmap;
 import java.util.*;
