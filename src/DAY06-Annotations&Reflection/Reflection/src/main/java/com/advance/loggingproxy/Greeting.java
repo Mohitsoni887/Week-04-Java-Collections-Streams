98dc5bfcd0a783ee1eb76395354d4d804f45a233
Mohitsoni887 <mohitsoni9886921@gmail.com>
2025-02-16 22:45:43 +0530

    [Mohit Soni] Add. Custom Logging Proxy Using Reflection

---
+++ src/DAY06-Annotations&Reflection/Reflection/src/main/java/com/advance/loggingproxy/Greeting.java
@@ -0,0 +1,5 @@
+package com.advance.loggingproxy;
+
+public interface Greeting {
+    String sayHello(String name);
+}
---
+++ src/DAY06-Annotations&Reflection/Reflection/src/main/java/com/advance/loggingproxy/GreetingImpl.java
@@ -0,0 +1,9 @@
+package com.advance.loggingproxy;
+
+
+public class GreetingImpl implements Greeting {
+    @Override
+    public String sayHello(String name) {
+        return "Hello, " + name + "!";
+    }
+}
