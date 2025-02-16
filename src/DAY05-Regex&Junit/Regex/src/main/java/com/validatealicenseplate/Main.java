a1d40d2ae0ca55d30152e9064d913958619057cb
Mohitsoni887 <mohitsoni9886921@gmail.com>
2025-02-16 23:08:08 +0530

    [Mohit Soni] Add.  Validate a License Plate Number

---
+++ src/DAY05-Regex&Junit/Regex/src/main/java/com/validatealicenseplate/Main.java
@@ -0,0 +1,11 @@
+package com.validatealicenseplate;
+
+public class Main {
+    public static void main(String[] args) {
+        // call the method and print
+        System.out.println(ValidateLicensePlateExample.isValidate("AB1234"));
+        System.out.println(ValidateLicensePlateExample.isValidate("AB12345"));
+
+
+    }
+}
